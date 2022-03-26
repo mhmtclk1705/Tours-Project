e39892625909f5b369454a5147093592e8e215ef
mhmtclk1705 <mehmetcelik1705@gmail.com>
2022-03-26 20:01:13 +0300

    edit

--- src/Tours.js
+++ src/Tours.js
@@ -4,12 +4,10 @@ const Tours = ({tours, removeTour}) => {
   return (
   <section>
     <div className="title">
-      {/* css style capitalize */}
       <h2>ours tours</h2>
       <div className="underline"></div>
       </div>
       <div>
-        {/* data içerisinde maple dolaştık sadece id kısmını aldık gerisini ...tour da bıraktık */}
         {tours.map((tour)=>{
           return (
             <Tour key = {tour.id} {...tour} removeTour = {removeTour}></Tour>
