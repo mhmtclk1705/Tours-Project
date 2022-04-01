304b389209c8ae7bddde627df2aa86e61932813f
mhmtclk1705 <mehmetcelik1705@gmail.com>
2022-04-02 01:12:01 +0300

    comment

--- src/App.js
+++ src/App.js
@@ -1,23 +1,36 @@
 import React, { useState, useEffect } from 'react'
 import Loading from './Loading'
 import Tours from './Tours'
+// ATTENTION!!!!!!!!!!
+// I SWITCHED TO PERMANENT DOMAIN
+// Fetch url adress
 const url = 'https://course-api.com/react-tours-project'
 function App() {
+  // fetch aşamasında loading ekranının ekranda çıkması için
   const [loading, setLoading] = useState(true);
+  // data
   const [tours,setTours] = useState([]);
   
+  // Tours un içerisinde not interested butonuna tıkladığımızda eğerki aynı id ye sahip değilse
+  // newTours a atıyoruz aynı id li olanı seçmeyecek ve silecek 
   const removeTour = (id) => {
     const newTours = tours.filter ((tour) => tour.id !== id);
     setTours(newTours);
+    // sonra da setTours ile tours un yeni hali newTours oluyor
   }
 
   const fetchTours = async () => {
+  // fetch aşamasında loading ekranının ekranda çıkması için
 
     setLoading(true);
     try {
+      // fetch aşaması
       const response = await fetch(url);
       const tours = await response.json();
+      // console.log(tours);  
+      // fetch sonrası setLoading false oluyor ekrandan gidiyor bilgiler ekrana geliyor
       setLoading(false)
+      // tours un içerisindekiler listeleniyor
       setTours(tours)
     } catch (error) {
       setLoading(false)
@@ -26,9 +39,11 @@ function App() {
     }
     
   }
+  // useEffect ile fetchTours başlatılıyor
   useEffect(() => {
     fetchTours();
   }, [])
+  // eğer loading useState i true olursa Loading componenti çalışsın
   if(loading){
     return(
       <main>
@@ -37,6 +52,8 @@ function App() {
     )
   }
 
+  // bütün tours içerisindekiler silinirse ekrana bir uyarı yazısı gelsin ve refresh butonu gelsin
+  // refresh butonuna tıklandığında fetchTours tekrar çalışsın ve yeniden bilgiler ekrana gelsin...
   if(tours.length === 0) {
     return <main>
       <div className="title">
@@ -45,6 +62,7 @@ function App() {
       </div>
     </main>
   }
+  // Tours componentinin içerisine data yı ve removeTour u props kullanarak gönderdik
   return <main>
     <Tours tours = {tours} removeTour = {removeTour}/>
   </main>
--- src/Tour.js
+++ src/Tour.js
@@ -1,8 +1,12 @@
 import React, { useState } from 'react';
+// destructuring ile data içerisindeki propertyleri tanımladık son olarak removeTour u da buraya ekledik butona
+// tıklandığında kullanılacağı için
 const Tour = ({id, image, info, price, name, removeTour}) => {
+  // readMore 200 karakterden sonrasını gizlemek için 
   const [readMore, setReadMore] = useState(false);
 
   return (
+    // css yapısı gelen datayı tagler içerisine yolluyoruz
   <article className='single-tour'>
     <img src={image} alt={name} />
     <footer>
@@ -10,9 +14,13 @@ const Tour = ({id, image, info, price, name, removeTour}) => {
         <h4>{name}</h4>
         <h4 className="tour-price">$ {price}</h4>
       </div>
+      {/* 200 karakterden fazlasını ... ile gösterip readmore butonu ile genişletme durumu */}
       <p>{readMore ? info : `${info.substring(0,200)}...`}
+      {/* eğer ki read more a basıldıysa show less ile 200 karaktere sınırlandırmak ve tam 
+      tersi durumda genişletmek için  */}
       <button onClick={() => setReadMore(!readMore)}>{readMore ? 'show less' : 'read more'}</button>
       </p>
+      {/* not interested a bastığımızda bastığımız tour u silmek için onCLick oluşturduk */}
       <button className='delete-btn' onClick={() => removeTour(id)}>not interested</button>
     </footer>
 
